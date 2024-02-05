b8258b6dc8ac679f9aba562614f953b0a5eaa202
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2024-02-05 15:55:08 +0100

    mapping a array of string dynamicly

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,17 @@
 function ListGroup() {
+  const items = ["New York", "San francisco", "Tokyo", "London", "Paris"];
   //ctrl+d to mark other occurencies of same word
   return (
     <>
       {/*telling react to wrap all of its children, so we dont need to use <div><div/> so we dont have another element*/}
-      <h1>List</h1>{" "}
+      <h1>List</h1>
       {/*h1 and ul are two different elements so we have to wrap to so it can be compiled to javascript*/}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+        {/*need specific key for each item, to keep track*/}
+        {/* have to wrap it to{} because JSX markup, because we can use there only html elements or react components*/}
       </ul>
     </>
   );
