16c0c7a5f45269fc2a1f2cc72c4e3920e05248f7
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2024-02-05 15:47:16 +0100

    Fragment usage

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,13 +1,18 @@
 function ListGroup() {
   //ctrl+d to mark other occurencies of same word
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      {/*telling react to wrap all of its children, so we dont need to use <div><div/> so we dont have another element*/}
+      <h1>List</h1>{" "}
+      {/*h1 and ul are two different elements so we have to wrap to so it can be compiled to javascript*/}
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
