fa8cf95b5d5239f329d40962e21dc2d12b19bee9
Jozef Sajtlava <jozef.sajtlava02@gmail.com>
2024-02-09 20:24:57 +0100

    Addind icons

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { FaCalendarTimes } from "react-icons/fa";
 import ListGroup from "./components/ListGroup"; //it will look for file index
 
 function App() {
   return (
     <div>
+      <FaCalendarTimes color="red"></FaCalendarTimes>
       <ListGroup items={['New York', 'Lost Angeles', 'San Francisco']} heading='Miami' onSelectItem={(item: string) => console.log(item)}></ListGroup>
     </div>
   );
