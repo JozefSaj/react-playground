bee3139009c594d3bfceb3ca0e10abc67ecc555d
Jozef Sajtlava <jozef.sajtlava02@gmail.com>
2024-02-10 12:08:05 -0500

    Understanding strict mode

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -4,7 +4,9 @@ import App from './App'
 // import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <React.StrictMode> {/*react renders each component twice* and takes the result of the second render
+                      first render is used primaly for detecting and reporting potential issues with our code
+                      second is used for actually update the user interface*/}
     <App />
   </React.StrictMode>,
 )
