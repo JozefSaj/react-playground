30d8413e4193111fce7fd8feecf79901f09adb12
Jozef Sajtlava <jozef.sajtlava02@gmail.com>
2024-02-11 05:37:51 -0500

    Updating Object

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -6,9 +6,14 @@ import ListGroup from "./components/ListGroup"; //it will look for file index
 import Like from "./components/Like";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
   return (
     <div>
-      <Like onClick={() => console.log("Clicked")}></Like>
+      {drink.price}
+      <button onClick={() => setDrink({...drink, price:6})}> </button>
     </div>
   );
 }
