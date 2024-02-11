08f071f6a379de878769a63307e4b415fbb5a06b
Jozef Sajtlava <jozef.sajtlava02@gmail.com>
2024-02-11 05:55:02 -0500

    Simplifying  Update Logic with Immer

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import { FaCalendarTimes } from "react-icons/fa";
 import ListGroup from "./components/ListGroup"; //it will look for file index
 import Like from "./components/Like";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -12,12 +13,17 @@ function App() {
   ]);
 
   const handleClick = () =>{
-    setBugs(bugs.map(bug => bug.id == 1 ? {...bug, fixed: true} : bug));
+    // setBugs(bugs.map(bug => bug.id == 1 ? {...bug, fixed: true} : bug));
+    setBugs(produce(draft => {
+      const bug = draft.find(bug => bug.id === 1);
+      if(bug) bug.fixed = true;
+    }))
   };
 
   return (
     <div>
-      <button> </button>
+      {bugs.map(bug => <p key = {bug.id}>{bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
