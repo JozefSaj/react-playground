c9219e40213cabb6e5c6a49de9c2db967b7fff2c
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2024-09-16 20:59:55 +0200

    Fetching Data

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,26 @@
-import ProductList from "./components/ProductList";
+import axios from "axios";
+import { set } from "immer/dist/internal";
 import React, { useEffect, useState } from "react";
 
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
-
-function App() {
-  useEffect(() => {
-    connect();
-
-    return () => {
-      disconnect();
-    };
-  });
-  return <div className=""></div>;
+interface User {
+  id: number;
+  name: string;
 }
 
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
 export default App;
